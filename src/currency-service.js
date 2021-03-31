1659d8a32dafa21743c5ec4cbcdebf2cfb2ac67c
marnionrails <37088724+marnionrails@users.noreply.github.com>
2021-03-30 23:37:45 -0400

    commit after successful postman test

--- src/currency-service.js
+++ src/currency-service.js
@@ -2,7 +2,7 @@ export default class Currency {
   static getCurrency() {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = ``;
+      const url = `https://v6.exchangerate-api.com/v6/59f92b2d26306ff939d3d766/latest/USD`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
