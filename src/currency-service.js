d88eea7615fb41bd584988c00be9a12f240460c3
marnionrails <37088724+marnionrails@users.noreply.github.com>
2021-03-30 23:25:08 -0400

    add dropdown menu for currency selection

--- src/currency-service.js
+++ src/currency-service.js
@@ -2,7 +2,7 @@ export default class Currency {
   static getCurrency() {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://api.nasa.gov/insight_weather/?api_key=${process.env.API_KEY}&feedtype=json&ver=1.0`;
+      const url = ``;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
